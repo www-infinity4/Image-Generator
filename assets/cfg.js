71de18df2915cea3c70d172dba8b31601a627d24
www-infinity4 <ephraim.nwo@gmail.com>
2026-08-12 20:35:53 -0500

    Add secure Image Studio assets/cfg.js

---
+++ assets/cfg.js
@@ -0,0 +1,6 @@
+/* Public, client-only configuration. Never place repository tokens or secrets in browser JavaScript. */
+window.IG_CONFIG = Object.freeze({
+  repository: "www-infinity4/Image-Generator",
+  clientOnly: true,
+  externalWritesEnabled: false
+});
